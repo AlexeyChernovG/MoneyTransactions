b49eada001b859efbd8a39a68fd90b75f8fee3c8
AlexeyChernovG <ikrevetko18@gmail.com>
2022-07-28 21:06:31 +0600

    Comission v.1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,14 @@
-fun main(args: Array<String>) {
-    
+fun main() {
+    val amount = 15.0
+
+    var comission = amount * 0.075
+
+    if (comission < 35) comission = 35.0
+
+    val amountPlusCom = amount + comission
+
+    println("Сумма перевода: " + amount + "р. ")
+    println("Комиссия: " + comission)
+    println("Итого: " + amountPlusCom)
+
 }
